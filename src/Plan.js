b742a4e4b0144c0b2449ba09b4cbc22637786678
AveryLucas <avery.lucas13@gmail.com>
2020-07-27 19:55:16 -0500

    mockup idea for flag based logic systems

--- src/Plan.js
+++ src/Plan.js
@@ -35,7 +35,8 @@ const unitStates = {
   // FLEEING: "fleeing", Out of Scope
 };
 
-// const building
+const unitTitles = [];
+const buildingTitles = [];
 
 const mockData = {
   units: [
@@ -65,4 +66,154 @@ const mockData = {
   //     spawnUnitCountdown: 5,
   //   },
   // ],
+};
+
+// Title System
+// - All units/buildings start off with the same stats. Titles are given to disguish units between each other.
+// - Titles can be leveled up for increasingly better advantages
+// | Example..
+
+const unit = {
+  // Position
+  x: 0,
+  y: 0,
+  // Behavior Logic
+  state: unitStates.IDLE,
+  target: null,
+  // Current Stats / Info
+  health: 100,
+  actionPoints: 2,
+  cooldown: 5,
+  speed: 4,
+  // Cooldown Periods
+  maxActionPoints: 2,
+  maxCooldownTime: 2,
+  maxResetTime: 10,
+};
+
+// const weapons = [{ id: 0, range: 0, damage: 10, name: "fist", hasProjectile: false }]
+
+// Tracked Stats
+// - Kills with weapon type
+// - Kills against team
+// - Kills with current title
+// - Kills with current weapon
+// - Times fleed from combat
+// - Times bitten by wearwolf/vampire...
+// - Times knocked unconscious
+// - Times sparred enemy death
+
+const mockTrackedStats = {
+  kills: { byType: { Sword: 10, Bow: 0 } },
+};
+
+// set something.another.else
+//
+
+// const damageLater = weaponProficiency * weaponDamage / defenderArmorReduction;
+// const hitChanceLater = undefined;
+const damageNow = weaponDamage;
+const hitChanceNow = 50;
+
+const unitTitles = [
+  {
+    name: "Swordsmen",
+    showTitle: false,
+    // requirements: null, // Requirements of null are titles that cannot be gained. (Typically starter titles)
+    // weaponAward: null, // Given once unit returns to a applicable building
+    leveled: [
+      {
+        requirements: { tracked: "kills.byType.Sword", condition: ">=5" },
+        series: "Untrained",
+        name: "Swordsmen",
+        showTitle: false,
+        // mods: { proficiency: },
+      },
+    ],
+  },
+  {
+    name: "Archer",
+    requirements: null,
+    weaponAward: 2,
+  },
+];
+
+// UNITS
+// BUILDINGS
+// WEAPONS
+// ALL LINKED
+
+// Unit flags..
+
+// @ location
+// - In_Building_InDestructible
+// - In_Building_Destructible
+// - In_Building_Friendly
+// - In_Building_Hostile
+
+// @ current state
+// - Attacking_Unit
+// - Recovering
+// - Hunting
+// - Fleeing
+
+// @ behavior traits
+// - Coward
+// - Fearless
+// - Building_Destroyer
+
+// Some flags cancel each other..
+// | For example.. a unit cant have both In_Building_Destructible and In_Building_InDestructible
+// | Or.. Coward and Fearless
+
+// Some flags come with stat improvements
+// Example.. Untrained_Archer comes with a status bonus of -1 to archery profeciency
+// Some flags can be leveled.
+
+const mockFlagData = {
+  Coward: {
+    name: "Coward",
+    showFlag: true,
+    overrideStats: { fleeAt: 0.8 },
+    modifiers: { actionPoints: "*2" },
+    check: (unit) => {
+      // Check if unit meets requirements to have this flag.
+      // if unit has fled 100% of battles and has participated in more then 5 battles
+    },
+    inCompatibleWith: [],
+  },
+  Fearless: {
+    name: "Fearless",
+    showFlag: true,
+    isState: false,
+    overrideStats: { fleeAt: 0.2 },
+    modifiers: { actionPoints: "*4" },
+    check: (unit) => {
+      // Check if unit meets requirements to have this flag.
+      // if unit has not fled 100% of battles and has participated in more then 15 battles
+    },
+    inCompatibleWith: [],
+  },
+  FightingLogic: {
+    name: "Fighting",
+    showFlag: true,
+    overrideStats: null,
+    modifiers: null,
+    check: null,
+    inCompatibleWith: null,
+    stateActions: {
+      fighting: (game, unit) => {
+        console.log("Do something!");
+      },
+    },
+  },
+};
+
+const unitStates = {
+  HUNTING: "hunting",
+  FIGHTING: "fighting",
+  RETURNING: "returning",
+  IDLE: "idle",
+  // RECOVERING: "recovering",
+  // FLEEING: "fleeing", Out of Scope
 };
